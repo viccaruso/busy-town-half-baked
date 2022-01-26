c37eec013e9fc62fc7a348f46bff4b17456e15ce
Victor Caruso <vic.caruso@gmail.com>
2022-01-26 15:54:59 -0800

    got one test for Vehicle component passing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Vehicle from './Vehicle';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders a 🚗 emoji somewhere on screen', () => {
+  render(<Vehicle type='car'/>);
+  const text = screen.getByText('🚗');
+  expect(text).toBeInTheDocument;
 });
+
